7a83216c3f183600aadc2a53e9ac5b612200fa7e
Curtis <chender93@gmail.com>
2020-08-22 12:57:31 -0400

    Added arrays for character selection

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//Variables for confirmations
+
+
+
+
+//Array for lowercase characters
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Array for uppercase characters
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//Array for numeric characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//Array for special characters
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 //Password length must be 8 characters, but no more than 128 characters
 var characterlenght = window.prompt("How many characters would you like in your password?");
 //Give the option for lowercase characters
