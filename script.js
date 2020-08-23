fc88445d7a11092f7bc5a46c691332817d206710
Curtis <chender93@gmail.com>
2020-08-23 19:19:07 -0400

    Changed approach from taking elements from an array to string

--- script.js
+++ script.js
@@ -4,65 +4,76 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+//String for lowercase characters
+var lower = ["abcdefghijklmnopqrstuvwxyz"];
+//String for uppercase characters
+var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+//String for numeric characters
+var number = ["123456789"];
+//String for special characters
+var special = ["!#$%&'*+-./:;<=>,?@[]^_`{|}~"];
+
 // Write password to the #password input
+//BEGIN writePassword function
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 //Variables for confirmations
-var submit;
-var confirmLower;
-var confrimUpper;
-var confirmNumber;
-var confirmSpecial;
+//Placeholder for password after defining
+var possiblePassword = "";
+
+
 
 //Variables for ultimate decision of characters
 var choices;
 
-
-
-//Array for lowercase characters
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//Array for uppercase characters
-upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//Array for numeric characters
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//Array for special characters
-special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-
 //Begin Password Generator Function
-function createPassword() {
 //Password length must be 8 characters, but no more than 128 characters
-var characterlength = window.prompt("How many characters would you like in your password?\n\r Password must be between 8 and 128 characters.");
-if (characterlength === false) {
+var characterLength = parseInt(prompt("How many characters would you like in your password?\n\r Password must be between 8 and 128 characters."));
+//console.log(characterLength);
+if (characterLength === false) {
   window.alert("You must specify the length of the password! Start again.");
-} else if (characterlength < 8 || characterlength > 128) {
+  } else if (characterLength < 8 || characterLength > 128) {
   window.alert("You must specify a length in between 8 and 128 characters! Start again.");
-} else {
+  return;
+  } else {
   //Give the option for lowercase characters
-  confirmLower = window.confirm("Would you like to include lowercase letters?\n\r Hit cancel for no.");
+   var confirmLower = window.confirm("Would you like to include lowercase letters?\n\r Hit cancel for no.");
   //Give the option for uppercase characters
-  confrimUpper = window.confirm("Would you like to include uppercase letters?\n\r Hit cancel for no.");
+  var confirmUpper = window.confirm("Would you like to include uppercase letters?\n\r Hit cancel for no.");
   //Give the option for numeric characters
-  confirmNumber = window.confirm("Would you like to include numbers?\n\r Hit cancel for no.");
+  var confirmNumber = window.confirm("Would you like to include numbers?\n\r Hit cancel for no.");
   //Give the option for special characters
-  confirmSpecial = window.confirm("Would you like to include special characters?\n\r Hit cancel for no.");
+  var confirmSpecial = window.confirm("Would you like to include special characters?\n\r Hit cancel for no.");
  };
-  //Ensure that each character type selected is included in the password
-  if (!confirmLower && !confirmNumber && !confrimUpper && !confirmSpecial) {
+  //Ensure that the user choose at least one option
+  if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
     choices = window.alert("You need to choose at least one option! Start Again");
+    return;
   }
+  
+  };
+  console.log(choices);
 
-}
+  
 
+  //For loop to select password length
+  for (var i = 0; i < characterLength; i++) {
+    var gatherChoices += choices(Math.floor(Math.random() * choices.length));
+  }
+ // console.log(gatherChoices);
+  //Password is displayed in an alert or written to the page
+ // var finalPassword = console.log(choices.join(""));
 
-//Password matches requested criteria
+  window.alert("You're new password is " + gatherChoices);
+  //Output password in the textbox
+//  function UserInput(ps) {
+//   document.getElementById("password").textContent = ps;
 
-//Password is displayed in an alert or written to the page
+//}
+var passwordText = document.querySelector("#password");
+
+passwordText.value = finalPassw;
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
